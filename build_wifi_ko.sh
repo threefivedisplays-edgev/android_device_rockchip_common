f0f0435d301da368b6b0bee00e2b16825aa6b60b
Alex Zhao <zzc@rock-chips.com>
2018-05-25 21:52:02 +0800

    copy wifi ko to /system/lib/modules
    Change-Id: Iccb6787a4861992fa9ad5c89560a0bbf9bd9673a
    Signed-off-by: Alex Zhao <zzc@rock-chips.com>

--- build_wifi_ko.sh
+++ build_wifi_ko.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
-
-. build/envsetup.sh >/dev/null && setpaths
-
-export PATH=$ANDROID_BUILD_PATHS:$PATH
-
-TARGET_PRODUCT=`get_build_var TARGET_PRODUCT`
-
-TARGET_ARCH=`get_build_var TARGET_ARCH`
-
-ANDROID_BUILD_TOP=`get_build_var ANDROID_BUILD_TOP`
-
-echo ANDROID_BUILD_TOP=$ANDROID_BUILD_TOP
-
-echo TARGET_PRODUCT=$TARGET_PRODUCT
-
-if [  "$TARGET_PRODUCT" == "rk3228h" ];then
-        TARGET_ARCH="arm64"
-fi
-echo TARGET_ARCH = $TARGET_ARCH
+TARGET_ARCH=$1
+TARGET_OUT=$2
 echo "---- make wifi ko  ----"
+echo "TARGET_ARCH = $TARGET_ARCH"
 
-make -C kernel ARCH=$TARGET_ARCH  modules -j18
-find $ANDROID_BUILD_TOP/kernel/drivers/net/wireless/rockchip_wlan/*  -name "*.ko" | \
-xargs -n1 -i cp {} $ANDROID_BUILD_TOP/vendor/rockchip/common/wifi/modules
+#make -C kernel ARCH=$TARGET_ARCH  modules -j8
+
+mkdir -p $TARGET_OUT/lib/modules
+
+find kernel/drivers/net/wireless/rockchip_wlan/*  -name "*.ko" | \
+xargs -n1 -i cp {} $TARGET_OUT/lib/modules/
+echo "Install wifi ko to $TARGET_OUT/lib/modules/"
